f04f7e4f40b1d1fd2fd04da3c0adcbbf504f4f86
tacosontitan <jkdavis1691@gmail.com>
2022-02-12 16:10:25 -0600

    Added hello world module.

---
+++ modules/HelloWorld.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Represents a basic module that says hello to the world.
+/// </summary>
+internal sealed class HelloWorld : ConsumableModule {
+    /// <summary>
+    /// Creates a new instance of the <see cref="HelloWorld" /> module.
+    /// </summary>
+    public HelloWorld() : base("hello", "Hello World", "Says hello to the world.") { }
+    public override void Invoke() => PostMessage("Hello world!");
+}
