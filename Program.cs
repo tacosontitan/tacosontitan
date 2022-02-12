743876e722cf0cdb14cebd08a551e373abf2a59f
tacosontitan <jkdavis1691@gmail.com>
2022-02-12 15:22:37 -0600

    Created basic loop.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿// Create a module service and register a handler to observe the error occurred event.
+var moduleService = new ModuleService();
+moduleService.ErrorOccurred += RecordModuleServiceError;
+
+// Discover any defined modules.
+moduleService.Discover();
+
+do {
+
+    // Prompt the user for a module.
+    Console.Write("Please enter `help` for help, `exit` to exit, or a command key: ");
+
+    // Store the user's response.
+    var response = Console.ReadLine();
+
+    // Determine if we should exit.
+    if (response?.Equals("exit", ignoreCase: true) == true)
+        break;
+
+    // Attempt to invoke a module.
+    if (moduleService.ContainsModule(response, out ConsumableModule module))
+        moduleService.Invoke(module);
+    else
+        Console.WriteLine($"No module found with the key `{response}`.");
+
+} while (true);
+
+
+#region Local Functions
+
+/// <summary>
+/// Records an error message received from the module service.
+/// </summary>
+/// <param name="sender">The sender of the event.</param>
+/// <param name="message">The error message that should be recorded.</param>
+void RecordModuleServiceError(object? sender, string message) => Console.WriteLine(message);
+
+#endregion
