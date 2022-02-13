4ad55f59b68fcfc848f271d9e29703e703aa1f85
tacosontitan <jkdavis1691@gmail.com>
2022-02-12 18:41:50 -0600

    Created buzz worker.

---
+++ modules/personal/chain-of-responsibility/fizz-buzz/BuzzWorker.cs
@@ -0,0 +1,31 @@
+namespace ChainOfResponsibility.FizzBuzz;
+
+/// <summary>
+/// Represents the `buzz` case of fizz buzz as a <see cref="WorkerProcess{T}" />.
+/// </summary>
+internal sealed class BuzzWorker : WorkerProcess<int> {
+    
+    #region Constructor
+
+    /// <summary>
+    /// Creates a new instance of <see cref="BuzzWorker" />.
+    /// </summary>
+    public BuzzWorker(WorkerProcess<int> processor) : base(processor) { }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// If the input is a multiple of 5 then output `buzz`, otherwise pass to the chain.
+    /// </summary>
+    public override void Process(int value) {
+        if (value % 5 == 0)
+            OnProcessingComplete("buzz");
+        else
+            base.Process(value);
+    }
+
+    #endregion
+
+}
