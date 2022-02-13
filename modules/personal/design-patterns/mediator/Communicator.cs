0d45af6bc59c01f72e037e20e429339dee5909df
tacosontitan <jkdavis1691@gmail.com>
2022-02-13 16:14:34 -0600

    Added basic properties, constructor, and summary tags.

--- modules/personal/design-patterns/mediator/Communicator.cs
+++ modules/personal/design-patterns/mediator/Communicator.cs
@@ -3,5 +3,41 @@ namespace Sandbox;
 /// Represents a <see cref="Communicator" /> implementation in the mediator pattern.
 /// </summary>
 public abstract class Communicator {
+
+    #region Properties
+
+    /// <summary>
+    /// The unique identifier for this <see cref="Communicator" /> instance.
+    /// </summary>
+    public Guid Id { get; private set; }
+    /// <summary>
+    /// The display name for this <see cref="Communicator" /> instance.
+    /// </summary>
+    public string DisplayName { get; private set; }
+    /// <summary>
+    /// The <see cref="Sandbox.Mediator" /> for this <see cref="Communicator" /> instance.
+    /// </summary>
+    protected Mediator Mediator { get; private set; }
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Creates a new instance of <see cref="Communicator" /> with the specified display name and <see cref="Sandbox.Mediator" /> instance.
+    /// </summary>
+    /// <param name="displayName">The display name for this <see cref="Communicator" /> instance.</param>
+    /// <param name="mediator">The <see cref="Sandbox.Mediator" /> for this <see cref="Communicator" /> instance.</param>
+    public Communicator(string displayName, Mediator mediator) {
+        Id = Guid.NewGuid();
+        DisplayName = displayName;
+        Mediator = mediator;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
     
 }
