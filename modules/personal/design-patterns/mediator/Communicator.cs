2b49cddba0e195b366f330e9b45cc84a9249a982
tacosontitan <jkdavis1691@gmail.com>
2022-02-12 22:22:22 -0600

    Created communicator shell the mediator pattern.

---
+++ modules/personal/design-patterns/mediator/Communicator.cs
@@ -0,0 +1,9 @@
+namespace Mediator;
+
+/// <summary>
+/// An abstract class representing a communicator.
+/// </summary>
+/// <typeparam name="T">The type of data this communicator uses.</typeparam>
+internal abstract class Communicator<T> {
+    
+}
