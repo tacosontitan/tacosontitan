ebb2b0ff8f2618d6787d0369d67b8c4fdcb6c4c4
tacosontitan <jkdavis1691@gmail.com>
2022-02-12 18:45:00 -0600

    Created fizz buzz module.

---
+++ modules/personal/chain-of-responsibility/FizzBuzz.cs
@@ -0,0 +1,8 @@
+namespace ChainOfResponsibility;
+
+/// <summary>
+/// Represents the popular programming test `fizz-buzz` in a way that implements the chain of responsibility design pattern; as a <see cref="ConsumableModule" />.
+/// </summary>
+internal sealed class FizzBuzz : ConsumableModule {
+
+}
