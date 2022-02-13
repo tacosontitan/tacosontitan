786ca00fd59636dc229c6038549a1056ad81bd54
tacosontitan <jkdavis1691@gmail.com>
2022-02-13 11:03:27 -0600

    Built out communicator, added mediator.

--- modules/personal/design-patterns/mediator/Communicator.cs
+++ modules/personal/design-patterns/mediator/Communicator.cs
@@ -3,7 +3,18 @@ namespace Mediator;
 /// <summary>
 /// An abstract class representing a communicator.
 /// </summary>
-/// <typeparam name="T">The type of data this communicator uses.</typeparam>
-internal abstract class Communicator<T> {
-    
+internal abstract class Communicator {
+    public Guid Id { get; private set; }
+    protected Mediator Mediator { get; private set; }
+    public Communicator(Mediator mediator) {
+        Id = Guid.NewGuid();
+        Mediator = mediator;
+    }
+    public void Send<T>(T data) => Mediator.Send(this, data);
+    public IEnumerable<TResponse> RequestInput<TRequest, TResponse>(TRequest request) {
+        foreach (TResponse response in Mediator.RequestInput<TRequest, TResponse>(this, request))
+            yield return response;
+    }
+    internal abstract void HandleIncomingData<T>(T data);
+    internal abstract TResponse HandleInputRequest<TRequest, TResponse>(TRequest request);
 }
---
+++ modules/personal/design-patterns/mediator/Mediator.cs
@@ -0,0 +1,25 @@
+namespace Mediator;
+
+internal sealed class Mediator {
+    private Dictionary<Guid, Communicator> _communicators = new();
+    public void Register(Communicator communicator) {
+        if (!_communicators.ContainsKey(communicator.Id))
+            _communicators.Add(communicator.Id, communicator);
+    }
+    public void Send<T>(Communicator sender, T data) {
+        foreach (var recipient in GetRecipients(sender))
+            recipient.HandleIncomingData(data);
+    }
+    public IEnumerable<TResponse> RequestInput<TRequest, TResponse>(Communicator sender, TRequest request) {
+        foreach (var recipient in GetRecipients(sender))
+            yield return recipient.HandleInputRequest<TRequest, TResponse>(request);
+    }
+
+    #region Private Methods
+
+    private IEnumerable<Communicator> GetRecipients(Communicator sender) => _communicators.Where(w => w.Key != sender.Id)
+                                                                                          .Select(s => s.Value);
+
+    #endregion
+
+}
