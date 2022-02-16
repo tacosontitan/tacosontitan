c8a90078722840cf4d54f6755383084f3002ae60
tacosontitan <jkdavis1691@gmail.com>
2022-02-16 14:47:28 -0600

    Added base data models.

---
+++ modules/personal/challenges/traveling-salesperson/models/City.cs
@@ -0,0 +1,5 @@
+internal class City {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public WorldCoordinate Coordinates { get; set; }
+}
---
+++ modules/personal/challenges/traveling-salesperson/models/WorldCoordinate.cs
@@ -0,0 +1,4 @@
+internal struct WorldCoordinate {
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
