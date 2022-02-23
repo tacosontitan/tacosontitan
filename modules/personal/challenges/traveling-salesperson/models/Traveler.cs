8921cc27a88feb5957fc34127d66ee9295d4754d
tacosontitan <jkdavis1691@gmail.com>
2022-02-22 22:57:51 -0600

    Created traveler shell.

---
+++ modules/personal/challenges/traveling-salesperson/models/Traveler.cs
@@ -0,0 +1,8 @@
+namespace Sandbox.Challenges.TravelingSalesperson;
+
+/// <summary>
+/// Represents a traveler in the traveling salesperson problem.
+/// </summary>
+internal sealed class Traveler {
+
+}
