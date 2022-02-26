776bb82abc694e3c8c02ed5c044f374fa73c47e7
tacosontitan <jkdavis1691@gmail.com>
2022-02-26 12:24:59 -0600

    Added onto path builder

--- modules/personal/challenges/traveling-salesperson/models/PathBuilder.cs
+++ modules/personal/challenges/traveling-salesperson/models/PathBuilder.cs
@@ -1,13 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
 namespace Sandbox.Challenges.TravelingSalesperson;
 
 /// <summary>
 /// Represents a <see cref="PathBuilder"/> object utilized to build paths across a collection of cities.
 /// </summary>
 internal abstract class PathBuilder {
+
+    #region Properties
+
+    /// <summary>
+    /// The cities this path builder uses to build paths.
+    /// </summary>
+    public IReadOnlyCollection<City> Cities { get; private set; }
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Creates a new instance of <see cref="PathBuilder"/> with the specified collection of cities.
+    /// </summary>
+    /// <param name="cities">The cities this path builder will use to build paths.</param>
+    public PathBuilder(IEnumerable<City> cities) => Cities = cities.ToList().AsReadOnly();
+
+    #endregion
     
     #region Public Methods
 
-    
+    /// <summary>
+    /// Generates all possible paths.
+    /// </summary>
+    /// <returns>Returns a collection of city paths.</returns>
+    public abstract IEnumerable<CityPath> GenerateAll();
 
     #endregion
 
