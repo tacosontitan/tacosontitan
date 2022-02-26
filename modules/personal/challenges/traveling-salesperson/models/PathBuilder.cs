789fcc82fa60e31f4bd996b2f4df0bc78bfbd786
tacosontitan <jkdavis1691@gmail.com>
2022-02-26 12:20:31 -0600

    Replaced traveler with path builder.

---
+++ modules/personal/challenges/traveling-salesperson/models/PathBuilder.cs
@@ -0,0 +1,14 @@
+namespace Sandbox.Challenges.TravelingSalesperson;
+
+/// <summary>
+/// Represents a <see cref="PathBuilder"/> object utilized to build paths across a collection of cities.
+/// </summary>
+internal abstract class PathBuilder {
+    
+    #region Public Methods
+
+    
+
+    #endregion
+
+}
--- modules/personal/challenges/traveling-salesperson/models/Traveler.cs
+++
@@ -1,8 +0,0 @@
-namespace Sandbox.Challenges.TravelingSalesperson;
-
-/// <summary>
-/// Represents a traveler in the traveling salesperson problem.
-/// </summary>
-internal sealed class Traveler {
-
-}
