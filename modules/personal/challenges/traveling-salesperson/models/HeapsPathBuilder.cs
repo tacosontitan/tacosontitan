093ca26e91bd51f6e0d13c3c0a0566b64318f449
tacosontitan <jkdavis1691@gmail.com>
2022-02-26 13:15:52 -0600

    Added path generation.

--- modules/personal/challenges/traveling-salesperson/models/HeapsPathBuilder.cs
+++ modules/personal/challenges/traveling-salesperson/models/HeapsPathBuilder.cs
@@ -1,13 +1,44 @@
 using System.Collections.Generic;
+using System.Linq;
+
 namespace Sandbox.Challenges.TravelingSalesperson;
 
 /// <summary>
 /// Represents a <see cref="PathBuilder"/> that utilizes Heap's algorithm to generate all permutations.
 /// </summary>
+/// <see href="https://en.wikipedia.org/wiki/Heap%27s_algorithm"/>
 internal sealed class HeapsPathBuilder : PathBuilder {
+
+    #region Constructor
+
+    /// <summary>
+    /// Creates a new instance of <see cref="HeapsPathBuilder"/> with the specified cities.
+    /// </summary>
+    /// <param name="cities">The cities this path builder will use to build paths.</param>
     public HeapsPathBuilder(IEnumerable<City> cities) : base(cities) { }
-    public override IEnumerable<CityPath> GenerateAll()
-    {
-        throw new System.NotImplementedException();
+
+    #endregion
+
+    #region Public Methods
+
+    public override IEnumerable<CityPath> GenerateAll() => GeneratePaths(Cities.Count() - 1, Cities.ToArray());
+
+    #endregion
+
+    #region Private Methods
+
+    private IEnumerable<CityPath> GeneratePaths(int n, City[] cities) {
+        if (n == 1)
+            yield return new CityPath(cities);
+        else {
+            for (int i = 0; i < n; i++) {
+                _ = GeneratePaths(n - 1, cities);
+                int swapIndex = n % 2 == 0 ? i : 0;
+                (cities[swapIndex], cities[n - 1]) = (cities[n - 1], cities[swapIndex]);
+            }
+        }
     }
+
+    #endregion
+
 }
