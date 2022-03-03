8ab620bdfd0a3a8e951969e64df5f2960e8ecde4
tacosontitan <jdavis@globallcs.com>
2022-03-02 18:13:54 -0600

    Added implementaton shell.

---
+++ modules/personal/challenges/ant-colony/models/NavigatorImplementation.cs
@@ -0,0 +1,7 @@
+namespace Sandbox.Challenges.AntColony;
+internal sealed class NavigatorImplementation : Navigator {
+    protected override List<Landmark> GeneratePath(List<Landmark> landmarks) {
+        // TODO: Generate a path 😁
+        return null;
+    }
+}
