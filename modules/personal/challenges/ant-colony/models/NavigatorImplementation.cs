6f0b48c6fcbf4ce4e171ceb3a2d3b1525622a751
tacosontitan <jdavis@globallcs.com>
2022-08-26 22:34:55 -0500

    Commented out implementation to repair compilation issues.

--- modules/personal/challenges/ant-colony/models/NavigatorImplementation.cs
+++ modules/personal/challenges/ant-colony/models/NavigatorImplementation.cs
@@ -1,7 +1,34 @@
-namespace Sandbox.Challenges.AntColony;
-internal sealed class NavigatorImplementation : Navigator {
-    protected override List<Landmark> GeneratePath(List<Landmark> landmarks) {
-        // TODO: Generate a path 😁
-        return null;
-    }
-}
+// namespace Sandbox.Challenges.AntColony;
+// internal sealed class NavigatorImplementation : Navigator {
+
+//     #region Fields
+
+//     private Dictionary<int, double> _landmarkPheromoneLevels = new Dictionary<int, double>();
+
+//     #endregion
+
+//     #region Protected Methods
+
+//     protected override List<Landmark> GeneratePath(List<Landmark> landmarks) {
+//         List<Landmark> result = new ();
+//         foreach (var landmark in landmarks) {
+            
+//         }
+//         return null;
+//     }
+
+//     #endregion
+
+//     #region Private Methods
+
+//     private void VisitLandmark(Landmark landmark) {
+//         double score = landmark.Value;
+//         Ant ant = new ();
+//         landmark.Visit(ant);
+//         if (ant.Alive)
+//             score = -score;
+//     }
+
+//     #endregion
+
+// }
