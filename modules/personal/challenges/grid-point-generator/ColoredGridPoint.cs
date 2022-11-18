bc2be339fffd0cc2d6b0aaa0c63d73dbc2c6d957
tacosontitan <jdavis@globallcs.com>
2022-11-18 12:29:12 -0600

    Created colored grid point class.

---
+++ modules/personal/challenges/grid-point-generator/ColoredGridPoint.cs
@@ -0,0 +1,10 @@
+namespace sandbox.modules.personal.challenges.gridpointgenerator
+{
+    public class ColoredGridPoint : GridPoint
+    {
+        /// <summary>
+        /// The numeric value representing the color of this <see cref="GridPoint"/>.
+        /// </summary>
+        public uint Color { get; set; }
+    }
+}
