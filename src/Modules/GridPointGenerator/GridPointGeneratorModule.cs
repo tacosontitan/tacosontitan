91932acff1b6ef5358354aa92e9e342de6507fee
tacosontitan <jkdavis1691@gmail.com>
2023-01-17 21:04:26 -0600

    Fixed grid point generator module.

--- src/Modules/GridPointGenerator/GridPointGeneratorModule.cs
+++ src/Modules/GridPointGenerator/GridPointGeneratorModule.cs
@@ -3,7 +3,7 @@ namespace Sandbox.Modules.GridPointGenerator
     /// <summary>
     /// Represents a <see cref="ConsumableModule"/> which generates a grid of a specified scale using specified colors.
     /// </summary>
-    public class GridPointGeneratorModule : ConsumableModule
+    internal sealed class GridPointGeneratorModule : Module
     {
         /// <summary>
         /// Creates a new <see cref="GridPointGeneratorModule"/> instance.
@@ -11,73 +11,74 @@ namespace Sandbox.Modules.GridPointGenerator
         public GridPointGeneratorModule() :
             base("grid", "Grid Point Generator", "Generates the points on a grid of N size.")
         { }
-        public override void Invoke()
+        protected override async Task Work()
         {
-            if (TryGetUserInput("What should the scale of the grid be?", out int gridSize))
-            {
-                if (gridSize is < 0 or > 5)
-                {
-                    PostMessage("Only grids between a scale of 1 and 5 are currently supported in the sandbox due to display limitations.");
-                    return;
-                } else if (TryGetUserInput("What should the color of event points be?", out ConsoleColor evenColor))
-                {
-                    if (TryGetUserInput("What should the color of odd points be?", out ConsoleColor oddColor))
-                    {
-                        ConsoleColor previousForegroundColor = Console.ForegroundColor;
-                        try
-                        {
-                            int scale = 0;
-                            foreach (ColoredGridPoint gridPoint in GenerateGrid(gridSize, evenColor, oddColor))
-                            {
-                                Console.ForegroundColor = gridPoint.Color;
-                                if (++scale > gridSize)
-                                {
-                                    scale = 1;
-                                    Console.Write(Environment.NewLine);
-                                }
+            // Request a grid size.
+            if (!TryRequestInput("What should the scale of the grid be?", out int gridSize))
+                return;
 
-                                Console.Write("██");
-                            }
-                        } catch (Exception e)
-                        {
-                            PostMessage(e.Message);
-                        } finally
-                        {
-                            Console.ForegroundColor = previousForegroundColor;
-                            Console.Write(Environment.NewLine);
-                        }
-                    }
-                }
+            // Validate the supplied grid size.
+            if (gridSize is < 1 or > 5)
+            {
+                await WriteError("Only grids between a scale of 1 and 5 are currently supported in the sandbox due to display limitations.");
+                return;
             }
+
+            // Request the grid colors.
+            if (!TryRequestInput("What should the color of even squares be?", out ConsoleColor evenColor) ||
+                !TryRequestInput("What should the color of odd squares be?", out ConsoleColor oddColor))
+                return;
+
+            ConsoleColor previousForegroundColor = Console.ForegroundColor;
+            try
+            {
+                int scale = 0;
+                foreach (ColoredGridPoint gridPoint in GenerateGrid(gridSize, evenColor, oddColor))
+                {
+                    Console.ForegroundColor = gridPoint.Color;
+                    if (++scale > gridSize)
+                    {
+                        scale = 1;
+                        Console.Write(Environment.NewLine);
+                    }
+
+                    Console.Write("██");
+                }
+            } catch (Exception e)
+            {
+                await WriteError(e.Message);
+            } finally
+            {
+                Console.ForegroundColor = previousForegroundColor;
+                Console.Write(Environment.NewLine);
+            }
+
+            await Task.CompletedTask;
         }
         /// <summary>
         /// Generates a grid with the specified scale using the specified colors.
         /// </summary>
         /// <param name="scale">The scale of the grid.</param>
-        /// <param name="colorA">The color to be used for even points.</param>
-        /// <param name="colorB">The color to be used for odd points.</param>
+        /// <param name="evenColor">The color to be used for even points.</param>
+        /// <param name="oddColor">The color to be used for odd points.</param>
         /// <returns>A collection of <see cref="ColoredGridPoint"/> instances representing the full grid.</returns>
-        private IEnumerable<ColoredGridPoint> GenerateGrid(int scale, ConsoleColor colorA, ConsoleColor colorB)
+        private IEnumerable<ColoredGridPoint> GenerateGrid(int scale, ConsoleColor evenColor, ConsoleColor oddColor)
         {
             int total = 0;
             for (int x = 0; x < scale; x++)
-            {
                 for (int y = 0; y < scale; y++)
-                {
-                    var gridPoint = new ColoredGridPoint
-                    {
-                        X = x,
-                        Y = y,
-                        Color = total++ % 2 == 0
-                            ? colorA
-                            : colorB
-                    };
-
-                    yield return gridPoint;
-                }
-            }
+                    yield return GenerateGridPoint(x, y, total++, evenColor, oddColor);
 
             yield break;
         }
+        private ColoredGridPoint GenerateGridPoint(int x, int y, int index, ConsoleColor evenColor, ConsoleColor oddColor) =>
+            new()
+            {
+                X = x,
+                Y = y,
+                Color = index % 2 == 0
+                            ? evenColor
+                            : oddColor
+            };
     }
 }
