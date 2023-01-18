2560b94cd82b0089f28e691fba54f25288427237
tacosontitan <jkdavis1691@gmail.com>
2023-01-17 21:18:33 -0600

    Created module request.

---
+++ src/ModuleRequest.cs
@@ -0,0 +1,21 @@
+using MediatR;
+using Sandbox;
+
+namespace Sandbox;
+
+/// <summary>
+/// Represents a request for a <see cref="Module"/>.
+/// </summary>
+public class ModuleRequest : IRequest<Module>
+{
+    /// <summary>
+    /// Represents the key of the module for invocation.
+    /// </summary>
+    public string Key { get; set; }
+    /// <summary>
+    /// Creates a new <see cref="ModuleRequest"/> instance.
+    /// </summary>
+    /// <param name="key">Represents the key of the module for invocation.</param>
+    public ModuleRequest(string key) =>
+        Key = key;
+}
