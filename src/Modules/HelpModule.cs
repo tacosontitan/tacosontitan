78b6922a0babda744c3a8f2b024cd5fb9b905588
tacosontitan <jkdavis1691@gmail.com>
2023-01-17 22:22:40 -0600

    Created help module.

---
+++ src/Modules/HelpModule.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mauve;
+
+namespace Sandbox;
+
+[Alias("help")]
+[Discoverable("Help", "Displays a list of modules available for execution.")]
+internal sealed class HelpModule : Module
+{
+    private readonly ModuleFactory _moduleFactory;
+    public HelpModule(ModuleFactory factory) :
+        base("Help") =>
+        _moduleFactory = factory;
+    protected override async Task Work()
+    {
+        foreach (ModuleDescription description in _moduleFactory.ModuleDescriptions)
+            await Write($"{description.Name}: {description.Description}");
+    }
+}
