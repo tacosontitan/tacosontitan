76948c95feed31f7c500c46d0fa69d9c51bdc2a0
tacosontitan <jkdavis1691@gmail.com>
2023-01-17 19:59:28 -0600

    Created input request.

---
+++ src/Runtime/InputRequest.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Sandbox.Runtime;
+
+/// <summary>
+/// Represents a request to get input from the user.
+/// </summary>
+internal sealed class InputRequest : IRequest<string>
+{
+    /// <summary>
+    /// The prompt to display to the user.
+    /// </summary>
+    public string Prompt { get; }
+    /// <summary>
+    /// Creates a new <see cref="InputRequest"/> instance.
+    /// </summary>
+    /// <param name="prompt">The prompt to display to the user.</param>
+    public InputRequest(string prompt) =>
+        Prompt = prompt;
+}
