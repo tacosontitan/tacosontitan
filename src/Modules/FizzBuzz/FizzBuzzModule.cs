b1f6b0d7b8d9f9309edf044fd81bb51b9d349b2d
tacosontitan <jkdavis1691@gmail.com>
2023-01-17 20:54:56 -0600

    Repaired fizz buzz module.

--- src/Modules/FizzBuzz/FizzBuzzModule.cs
+++ src/Modules/FizzBuzz/FizzBuzzModule.cs
@@ -3,10 +3,10 @@ namespace Sandbox.Modules.FizzBuzz;
 /// <summary>
 /// Represents the popular programming test `fizz-buzz` in a way that implements the chain of responsibility design pattern; as a <see cref="ConsumableModule" />.
 /// </summary>
-internal sealed class FizzBuzz : ConsumableModule
+internal sealed class FizzBuzzModule : Module
 {
-    public FizzBuzz() : base("fizz", "Fizz Buzz", "Demonstrates the chain of responsibility pattern using fizz-buzz.") { }
-    public override void Invoke()
+    public FizzBuzzModule() : base("fizz", "Fizz Buzz", "Demonstrates the chain of responsibility pattern using fizz-buzz.") { }
+    protected override async Task Work()
     {
         // Create the chain; though it seems backwards, think about it from an execution standpoint.
         //      Invocation begins with the `fizz-buzz` condition,
@@ -14,10 +14,13 @@ internal sealed class FizzBuzz : ConsumableModule
         var outputProcessor = new BasicHandler();
         var fizzProcessor = new FizzHandler(outputProcessor);
         var buzzProcessor = new BuzzHandler(fizzProcessor);
-        var fizzBuzzProcessor = new FizzBuzzProcessor(buzzProcessor);
+        var fizzBuzzProcessor = new FizzBuzzHandler(buzzProcessor);
 
-        // Subscribe to the worker events.
-        static void ChainProcessingComplete(object? sender, object data) => PostMessage(data?.ToString() ?? string.Empty);
+        // Handle processing complete event.
+        void ChainProcessingComplete(object? sender, object data) =>
+            _ = Write(data?.ToString() ?? string.Empty);
+
+        // Subscibe to events.
         outputProcessor.ProcessingComplete += ChainProcessingComplete;
         fizzProcessor.ProcessingComplete += ChainProcessingComplete;
         buzzProcessor.ProcessingComplete += ChainProcessingComplete;
@@ -26,5 +29,7 @@ internal sealed class FizzBuzz : ConsumableModule
         // Run through a few integers to test it.
         for (int i = 0; i <= 15; i++)
             fizzBuzzProcessor.Process(i);
+
+        await Task.CompletedTask;
     }
 }
