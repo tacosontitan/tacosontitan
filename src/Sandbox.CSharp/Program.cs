2743ba42d26de539000f969fd202f49852047f02
tacosontitan <jkdavis1691@gmail.com>
2023-07-19 18:56:02 -0500

    Simplified launch configurations.

--- src/Sandbox.CSharp/Program.cs
+++ src/Sandbox.CSharp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, world!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("📦 Starting sandbox.");
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(Configure)
+    .Build();
+
+host.Run();
+
+void Configure(IServiceCollection services)
+{
+
+}
