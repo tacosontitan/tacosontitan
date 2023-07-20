e8ff87978ba42b163c6b803eeff0e5dbc674d96a
tacosontitan <jkdavis1691@gmail.com>
2023-07-19 19:21:34 -0500

    Updated entry point to enable configuration.

--- src/Sandbox.CSharp/Program.cs
+++ src/Sandbox.CSharp/Program.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("📦 Starting sandbox.");
 IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(ConfigureAppConfiguration)
+    .ConfigureLogging(ConfigureLogging)
     .ConfigureServices(Configure)
     .Build();
 
 host.Run();
+
+void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder config)
+{
+    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+    config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true);
+    config.AddEnvironmentVariables();
+    if (args != null)
+        config.AddCommandLine(args);
+}
+
+void ConfigureLogging(HostBuilderContext context, ILoggingBuilder logging)
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+    logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+}
 
 void Configure(IServiceCollection services)
 {
