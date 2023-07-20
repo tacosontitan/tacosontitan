fbf444a0e33b6f3ed39b63a939343c07c5c68050
tacosontitan <jkdavis1691@gmail.com>
2023-07-20 17:36:24 -0500

    Created module extensions to cleanup entry.

---
+++ src/Sandbox.CSharp/Modules/ModuleExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Sandbox.CSharp.Core;
+
+namespace Sandbox.CSharp.Modules;
+
+/// <summary>
+/// Defines extension methods for registering modules.
+/// </summary>
+public static class ModuleExtensions
+{
+    /// <summary>
+    /// Adds the modules to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the modules to.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+    public static IServiceCollection AddModules(this IServiceCollection services) => services
+        .AddCoreModules()
+        .AddModule<HelloWorld>()
+        .AddModule<AsyncEnumerable>();
+}
