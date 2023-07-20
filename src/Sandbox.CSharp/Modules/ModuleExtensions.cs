21b8f1731776f3fa15eb79ca3a7740cab6177cc1
tacosontitan <jkdavis1691@gmail.com>
2023-07-20 17:45:23 -0500

    Added paginator module.

--- src/Sandbox.CSharp/Modules/ModuleExtensions.cs
+++ src/Sandbox.CSharp/Modules/ModuleExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Sandbox.CSharp.Core;
+using Sandbox.CSharp.Modules.Paginator;
 
 namespace Sandbox.CSharp.Modules;
 
@@ -19,6 +20,7 @@ public static class ModuleExtensions
     /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
     public static IServiceCollection AddModules(this IServiceCollection services) => services
         .AddCoreModules()
+        .AddPaginatorModule()
         .AddModule<HelloWorld>()
         .AddModule<AsyncEnumerable>();
 }
