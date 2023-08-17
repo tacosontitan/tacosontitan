4d5e2b97f2c1987879b222a290520e36409287ed
tacosontitan <jkdavis1691@gmail.com>
2023-08-16 19:59:30 -0500

    Updated terminal entry point.

---
+++ src/Sandbox.Terminal/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Sandbox.Modules;
+using Sandbox.Terminal;
+
+Console.WriteLine("Starting sandbox.");
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(ConfigureAppConfiguration)
+    .ConfigureLogging(ConfigureLogging)
+    .ConfigureServices(Configure)
+    .Build();
+
+host.Run();
+
+void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder config)
+{
+    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+    config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true);
+    config.AddEnvironmentVariables();
+    if (args != null)
+        config.AddCommandLine(args);
+}
+
+void ConfigureLogging(HostBuilderContext context, ILoggingBuilder logging)
+{
+    logging.ClearProviders();
+    logging.AddDebug();
+
+    IConfigurationSection loggingConfiguration = context.Configuration.GetSection("Logging");
+    if (loggingConfiguration is null)
+        return;
+
+    logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+}
+
+void Configure(IServiceCollection services)
+{
+    services
+        .AddTerminal()
+        .AddModules();
+}
