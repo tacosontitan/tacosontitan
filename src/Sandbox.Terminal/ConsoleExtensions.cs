5434006c343afd0f636da5096e01e158d298dc74
tacosontitan <jkdavis1691@gmail.com>
2023-08-16 19:59:20 -0500

    Ported terminal extensions.

---
+++ src/Sandbox.Terminal/ConsoleExtensions.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+using Sandbox.Core;
+
+namespace Sandbox.Terminal;
+
+/// <summary>
+/// Defines extension methods for simplifying console injection.
+/// </summary>
+public static class ConsoleExtensions
+{
+    /// <summary>
+    /// Adds terminal services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <returns>The <see cref="IServiceCollection"/> with console services added.</returns>
+    public static IServiceCollection AddTerminal(this IServiceCollection services) => services
+        .AddTransient<IConsumerService, TerminalConsumer>()
+        .AddHostedService<TriggerService>();
+}
