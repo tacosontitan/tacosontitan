57c0c41df9996655bbdf2a3058ef101fe71c040e
tacosontitan <jkdavis1691@gmail.com>
2023-12-11 10:26:07 -0600

    Created MAUI presentation project.

---
+++ src/Sandbox.Presentation.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Sandbox.Presentation.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/Sandbox.Presentation.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Sandbox.Presentation.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
