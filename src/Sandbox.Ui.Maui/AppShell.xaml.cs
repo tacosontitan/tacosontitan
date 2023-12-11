38bca0669def28a522cf422e095038006d7cdbd5
tacosontitan <jkdavis1691@gmail.com>
2023-12-11 10:19:50 -0600

    Removed MAUI project.

--- src/Sandbox.Ui.Maui/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sandbox.Ui;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- src/Sandbox.Ui.Maui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Sandbox.Ui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
