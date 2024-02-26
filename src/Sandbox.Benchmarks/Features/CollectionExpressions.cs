fde746702a4ff5f511480fd00aa8f6d5e607d0f2
Taco <65432314+tradinglounge@users.noreply.github.com>
2024-02-26 10:55:53 -0600

    Suppressed CA1822.

--- src/Sandbox.Benchmarks/Features/CollectionExpressions.cs
+++ src/Sandbox.Benchmarks/Features/CollectionExpressions.cs
@@ -8,15 +8,18 @@ public class CollectionExpressions
 {
     [Benchmark]
     [SuppressMessage("ReSharper", "UseCollectionExpression", Justification = "This is a benchmark for collection expressions.")]
+    [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Benchmark methods cannot be static.")]
     public List<string> InitializationSyntax() =>
         new() { "foo", "bar", "baz" };
     
     [Benchmark]
     [SuppressMessage("ReSharper", "UseCollectionExpression", Justification = "This is a benchmark for collection expressions.")]
+    [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Benchmark methods cannot be static.")]
     public List<string> InitializationSyntaxWithCapacity() =>
         new(capacity: 3) { "foo", "bar", "baz" };
 
     [Benchmark]
+    [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Benchmark methods cannot be static.")]
     public List<string> ExpressionSyntax() =>
             ["foo", "bar", "baz"];
 }
