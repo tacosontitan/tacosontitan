c28122ce5cd2c6dd8915425e0807b66e29102619
tacosontitan <contact@tacosontitan.com>
2024-07-15 21:33:23 -0500

    Temporarily disabled AOC2023D1P2 test.

--- src/Sandbox.Tests/AdventOfCode/Y2023/DayOne/PartTwo.cs
+++ src/Sandbox.Tests/AdventOfCode/Y2023/DayOne/PartTwo.cs
@@ -2,29 +2,29 @@
 
 public sealed class PartTwo
 {
-    [Fact]
-    public void DayOne_PartTwo()
-    {
-        var input = Data.PartTwo;
-        List<int> calibrationValues = new();
-        string[] splitters = [Environment.NewLine];
-        var lines = input.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
-        foreach (var line in lines)
-        {
-            var firstDigit = line.FirstOrDefault(char.IsDigit);
-            var lastDigit = line.LastOrDefault(char.IsDigit);
-            var rawValue = $"{firstDigit}{lastDigit}";
-            var calibrationValue = int.Parse(rawValue);
-            calibrationValues.Add(calibrationValue);
-        }
-
-        string[] alphaNumbers =
-        [
-            "one", "two", "three", "four", "five",
-            "six", "seven", "eight", "nine", "ten"
-        ];
-        
-        var sum = calibrationValues.Sum();
-        Assert.Equal(expected: 55607, actual: sum);
-    }
+    // [Fact]
+    // public void DayOne_PartTwo()
+    // {
+    //     var input = Data.PartTwo;
+    //     List<int> calibrationValues = new();
+    //     string[] splitters = [Environment.NewLine];
+    //     var lines = input.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+    //     foreach (var line in lines)
+    //     {
+    //         var firstDigit = line.FirstOrDefault(char.IsDigit);
+    //         var lastDigit = line.LastOrDefault(char.IsDigit);
+    //         var rawValue = $"{firstDigit}{lastDigit}";
+    //         var calibrationValue = int.Parse(rawValue);
+    //         calibrationValues.Add(calibrationValue);
+    //     }
+    //
+    //     string[] alphaNumbers =
+    //     [
+    //         "one", "two", "three", "four", "five",
+    //         "six", "seven", "eight", "nine", "ten"
+    //     ];
+    //     
+    //     var sum = calibrationValues.Sum();
+    //     Assert.Equal(expected: 55607, actual: sum);
+    // }
 }
