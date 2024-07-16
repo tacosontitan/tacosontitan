ee4ea6ffb344498b7841c86678710c6e04d4e2de
tacosontitan <contact@tacosontitan.com>
2024-07-15 21:25:04 -0500

    Cleaned up quick tests.

---
+++ src/tests/AdventOfCode/Y2023/DayOne/PartOne.cs
@@ -0,0 +1,24 @@
+﻿namespace Sandbox.QuickTests.AdventOfCode.Y2023.DayOne;
+
+public sealed class PartOne
+{
+    [Fact]
+    public void DayOne_PartOne()
+    {
+        var input = Data.PartOne;
+        List<int> calibrationValues = new();
+        string[] splitters = [Environment.NewLine];
+        var lines = input.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            var firstDigit = line.FirstOrDefault(char.IsDigit);
+            var lastDigit = line.LastOrDefault(char.IsDigit);
+            var rawValue = $"{firstDigit}{lastDigit}";
+            var calibrationValue = int.Parse(rawValue);
+            calibrationValues.Add(calibrationValue);
+        }
+
+        var sum = calibrationValues.Sum();
+        Assert.Equal(expected: 55607, actual: sum);
+    }
+}
---
+++ src/tests/AdventOfCode/Y2023/DayOne/PartTwo.cs
@@ -0,0 +1,30 @@
+﻿namespace Sandbox.QuickTests.AdventOfCode.Y2023.DayOne;
+
+public sealed class PartTwo
+{
+    [Fact]
+    public void DayOne_PartTwo()
+    {
+        var input = Data.PartTwo;
+        List<int> calibrationValues = new();
+        string[] splitters = [Environment.NewLine];
+        var lines = input.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            var firstDigit = line.FirstOrDefault(char.IsDigit);
+            var lastDigit = line.LastOrDefault(char.IsDigit);
+            var rawValue = $"{firstDigit}{lastDigit}";
+            var calibrationValue = int.Parse(rawValue);
+            calibrationValues.Add(calibrationValue);
+        }
+
+        string[] alphaNumbers =
+        [
+            "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "ten"
+        ];
+        
+        var sum = calibrationValues.Sum();
+        Assert.Equal(expected: 55607, actual: sum);
+    }
+}
