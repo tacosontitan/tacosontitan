54a610c61dfdc1ad565e7e89b3fcdae07a400a47
tacosontitan <contact@tacosontitan.com>
2024-08-23 15:22:40 -0400

    Removed advent of code.
    The advent of code problems will be tackled as part of this repo when time permits, but through a separate test project. Given that only the first part of the first problem for 2023 was solved, I'm okay scrapping the entire setup for it until I have time to move on it again.

--- src/Sandbox.Tests/AdventOfCode/Y2023/DayOne/Overview.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Immutable;
-
-namespace Sandbox.QuickTests.AdventOfCode.Y2023.DayOne;
-
-/// <see href="https://adventofcode.com/2023/day/1"/>
-public sealed partial class Trebuchet;
