894d0d1d413e29ed00815b2d70de9a501d5dacee
tacosontitan <contact@tacosontitan.com>
2025-11-20 14:19:09 -0600

    Add tailwind

--- src/apps/portfolio/src/App.tsx
+++ src/apps/portfolio/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className='placeholder'>
-      <h1>Portfolio Placeholder</h1>
+      <h1 className="text-3xl font-bold underline">Portfolio Placeholder</h1>
       <p>
         Your modern TypeScript/React portfolio will go here!
       </p>
