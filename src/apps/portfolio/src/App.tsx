c886b870a3d9a502246eb222b1018231820095c5
tacosontitan <contact@tacosontitan.com>
2025-11-20 11:00:46 -0600

    Create placeholder for portfolio ui

---
+++ src/apps/portfolio/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        background: '#18191A',
+        color: '#F4F6F8',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <h1>Portfolio Placeholder</h1>
+      <p>
+        Your modern TypeScript/React portfolio will go here!
+      </p>
+    </div>
+  );
+};
+
+export default App;
